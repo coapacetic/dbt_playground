939aa850edd087339ab92d423f803585d4f870f9
Kyle Coapman <kcoapman@fishtownanalytics.com>
2024-01-29 17:46:16 +0000

    quick tweak to generate schema name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
+{% macro generate_schema_name(custom_schema, node) %}
 
     {%- set default_schema = target.schema -%}
-    {%- if target.name == 'prod' and custom_schema_name is not none -%}
+    {%- if target.name == 'default' and custom_schema_name is not none -%}
 
         {{ custom_schema_name | trim }}
 
